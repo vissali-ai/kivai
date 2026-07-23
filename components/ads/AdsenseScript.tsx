8a426dff0a3773d86d8b9ebd0bda34989910a624
Marcus Vissali <vissali@gmail.com>
2026-07-22 22:57:15 -0300

    Configura infraestrutura inicial do Google AdSense

--- components/ads/AdsenseScript.tsx
+++ components/ads/AdsenseScript.tsx
@@ -5,32 +5,16 @@ import Script from "next/script";
 export default function AdsenseScript() {
   const client = process.env.NEXT_PUBLIC_ADSENSE_CLIENT;
 
-  console.log("ADSENSE CLIENT:", client);
+  if (!client) {
+    return null;
+  }
 
   return (
-    <>
-      <div
-        style={{
-          position: "fixed",
-          bottom: 10,
-          right: 10,
-          zIndex: 99999,
-          background: "red",
-          color: "white",
-          padding: "8px",
-          fontSize: "12px",
-        }}
-      >
-        ADSENSE: {client || "UNDEFINED"}
-      </div>
-
-      {client && (
-       <Script
-  id="google-adsense"
-  strategy="afterInteractive"
-  src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client}`}
-/>
-      )}
-    </>
+    <Script
+      id="google-adsense"
+      strategy="afterInteractive"
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client}`}
+      crossOrigin="anonymous"
+    />
   );
 }
