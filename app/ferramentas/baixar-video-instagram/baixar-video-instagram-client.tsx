76ca9302699095b80bc44d137069f4fa39750322
vissali-ai <vissali@gmail.com>
2026-09-02 13:02:45 -0300

    Ajusta detecção mobile no downloader do Instagram

--- app/ferramentas/baixar-video-instagram/baixar-video-instagram-client.tsx
+++ app/ferramentas/baixar-video-instagram/baixar-video-instagram-client.tsx
@@ -61,7 +61,7 @@ async function responseMessage(response: Response) {
 
 function isMobileDevice() {
   if (typeof navigator === "undefined") return false;
-  return /Android|iPhone|iPad|iPod|Mobile/i.test(navigator.userAgent);
+  return /Android|iPhone|iPad|iPod|Mobile/i.test(navigator.userAgent) || (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
 }
 
 function canShareFiles() {
